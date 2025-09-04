19cc8994fd4b50c50754bf6883b7e62726defd95
s.zebzeev <sergey.zebzeev@gmail.com>
2025-09-05 00:15:51 +0500

    expand.ts - expandbtn меняет иконку взависимости от состояния

--- src/components/modal/action_bar/buttons/expand.ts
+++ src/components/modal/action_bar/buttons/expand.ts
@@ -1,11 +1,26 @@
 import {createIconButton} from "../utils";
+import {setIcon} from "obsidian";
 
 interface ExpandComponentProps {
 	onClick: () => void;
 }
 
+enum ExpandComponentState {
+	CLOSED = "CLOSED",
+	OPENED = "OPENED",
+}
+
 export function createExpandComponent(actionBarContainer: HTMLElement, props: ExpandComponentProps) {
-	createIconButton("menu-button", "tornado", actionBarContainer).onClickEvent(ev => {
+	const button = createIconButton("menu-button", "cloud-fog", actionBarContainer);
+	button.dataset.state = ExpandComponentState[ExpandComponentState.CLOSED]
+
+	button.onClickEvent(ev => {
+		const isClosed = button.dataset.state === ExpandComponentState.CLOSED;
+		const span = button.querySelector('span') as HTMLElement;
+
+		button.dataset.state = isClosed ? ExpandComponentState.OPENED : ExpandComponentState.CLOSED;
+		setIcon(span, isClosed ? 'cloud' : 'cloud-fog');
+
 		props.onClick();
 	})
 }
